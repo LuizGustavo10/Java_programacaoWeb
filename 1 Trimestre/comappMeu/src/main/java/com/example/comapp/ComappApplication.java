2b4593831c04bbf6bf79609b21c6714e52aeb71d
Luiz Gustavo <37215681+Gu3001@users.noreply.github.com>
2019-05-28 12:13:56 -0300

    Trabalho de PW, finalmente terminado
    todas as funções operantes

---
+++ 1 Trimestre/comappMeu/src/main/java/com/example/comapp/ComappApplication.java
@@ -0,0 +1,13 @@
+package com.example.comapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComappApplication.class, args);
+	}
+
+}
