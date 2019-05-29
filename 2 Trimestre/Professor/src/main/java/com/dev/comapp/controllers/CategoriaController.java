b5f338a050640a1b805535253b2fb6e1d49c4422
Luiz Gustavo <37215681+Gu3001@users.noreply.github.com>
2019-05-28 22:35:39 -0300

    Projeto professor

---
+++ 2 Trimestre/Professor/src/main/java/com/dev/comapp/controllers/CategoriaController.java
@@ -0,0 +1,74 @@
+package com.dev.comapp.controllers;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dev.comapp.models.Categoria;
+import com.dev.comapp.repository.CategoriaRepository;
+
+
+
+@Controller
+public class CategoriaController {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping("/categoria")
+	public ModelAndView findAll() {
+		
+		ModelAndView mv = new ModelAndView("/categoriaPost");
+		mv.addObject("categorias", repository.findAll());
+		
+		return mv;
+	}
+	
+	@GetMapping("/addCategoria")
+	public ModelAndView add(Categoria categoria) {
+		
+		ModelAndView mv = new ModelAndView("/categoriaAdd");
+		mv.addObject("categoria", categoria);
+		
+		return mv;
+	}
+	
+	@GetMapping("/editarCategoria/{id}")
+	public ModelAndView edit(@PathVariable("id") Long id) {
+		
+		Optional<Categoria> categoria = repository.findById(id);
+		Categoria e = categoria.get();	
+		
+		return add(e);
+	}
+	
+	@GetMapping("/removerCategoria/{id}")
+	public ModelAndView delete(@PathVariable("id") Long id) {
+		
+		Optional<Categoria> categoria = repository.findById(id);
+		Categoria e = categoria.get();
+		repository.delete(e);	
+		
+		return findAll();
+	}
+
+	@PostMapping("/salvarCategoria")
+	public ModelAndView save(@Valid Categoria categoria, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return add(categoria);
+		}
+		
+		repository.saveAndFlush(categoria);
+		
+		return findAll();
+	}
+	
+}
